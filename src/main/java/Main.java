4e911a71baece00b4fceb5496bdf0c19dc6be21d
franciscoK044 <frankrauel99@gmail.com>
2024-09-02 15:39:55 -0300

    Agrego archivo pom

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,9 +2,10 @@ import factory.ConexionFactory;
 import factory.DBFactory;
 
 import java.sql.Connection;
+import java.sql.SQLException;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
 
         //Seleccion de la base de datos a usar(mysql o derby)
         ConexionFactory factory= DBFactory.getDB("","","","");
