dec049a32a6e3e2412cd61a277852a45b80438f4
lmq94 <quiroga9409@gmail.com>
2024-09-04 08:18:15 -0300

    Resuelvo ultimos problemas con la entidad factura_producto

--- src/main/java/factory/MySQLImplementacion.java
+++ src/main/java/factory/MySQLImplementacion.java
@@ -6,7 +6,7 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class MySQLImplementacion implements ConexionFactory{
+public class MySQLImplementacion implements ConexionFactory {
     private String url;
     private String usuario;
     private String contrasena;
@@ -19,36 +19,44 @@ public class MySQLImplementacion implements ConexionFactory{
 
     @Override
     public Connection crearConexion() throws SQLException {
-        return Conexion.getInstancia(url,usuario,contrasena).getConexion();
+        return Conexion.getInstancia(url, usuario, contrasena).getConexion();
     }
 
     @Override
     public void crearTablas(Connection conexion) throws SQLException {
-            try (Statement statement = conexion.createStatement()) {
-                    String queryCliente = "CREATE TABLE IF NOT EXISTS cliente("+"idCliente INT," +
-                            "nombre VARCHAR(500)," +
-                            "email VARCHAR(150)," +
-                            "PRIMARY KEY(idCliente))";
+        try (Statement statement = conexion.createStatement()) {
+            String queryCliente = "CREATE TABLE IF NOT EXISTS cliente(" +
+                    "idCliente INT AUTO_INCREMENT," +
+                    "nombre VARCHAR(500)," +
+                    "email VARCHAR(150)," +
+                    "PRIMARY KEY(idCliente))";
 
-                    String queryFactura=" CREATE TABLE IF NOT EXISTS factura(" +"idFactura INT," +
-                            "idCliente INT," +
-                            "PRIMARY KEY(idFactura)," +
-                            "FOREIGN KEY(idCliente) REFERENCES cliente(idCliente))";
-                    String queryProducto = "CREATE TABLE IF NOT EXISTS producto("+"idProducto INT," +
-                            "nombre VARCHAR(45)," +
-                            "valor FLOAT," +
-                            "PRIMARY KEY(idProducto))";
-                    String queryfactura_producto = "CREATE TABLE IF NOT EXISTS factura_producto("+"idFactura INT," +
-                            "idProducto INT," +
-                            "cantidad INT," +
-                            "FOREIGN KEY(idFactura,idProducto) REFERENCES factura(idFactura),producto(idProducto))";
+            String queryFactura = "CREATE TABLE IF NOT EXISTS factura(" +
+                    "idFactura INT AUTO_INCREMENT," +
+                    "idCliente INT," +
+                    "PRIMARY KEY(idFactura)," +
+                    "FOREIGN KEY(idCliente) REFERENCES cliente(idCliente))";
 
-                    statement.execute(queryCliente);
-                    statement.execute(queryFactura);
-                    statement.execute(queryProducto);
-                    statement.execute(queryfactura_producto);
-            }catch (SQLException e){
-                throw new SQLException(e);
-            }
+            String queryProducto = "CREATE TABLE IF NOT EXISTS producto(" +
+                    "idProducto INT AUTO_INCREMENT," +
+                    "nombre VARCHAR(45)," +
+                    "valor FLOAT," +
+                    "PRIMARY KEY(idProducto))";
+
+            String queryFacturaProducto = "CREATE TABLE IF NOT EXISTS factura_producto(" +
+                    "idFactura INT," +
+                    "idProducto INT," +
+                    "cantidad INT," +
+                    "PRIMARY KEY(idFactura, idProducto)," +
+                    "FOREIGN KEY(idFactura) REFERENCES factura(idFactura)," +
+                    "FOREIGN KEY(idProducto) REFERENCES producto(idProducto))";
+
+            statement.execute(queryCliente);
+            statement.execute(queryFactura);
+            statement.execute(queryProducto);
+            statement.execute(queryFacturaProducto);
+        } catch (SQLException e) {
+            throw new SQLException(e);
+        }
     }
 }
