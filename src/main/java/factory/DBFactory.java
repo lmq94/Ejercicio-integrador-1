d9b4565be7dcb9b4ae392184bcc5f5a53f3e0075
lmq94 <quiroga9409@gmail.com>
2024-09-09 23:44:39 -0300

    Refactorizo switch

--- src/main/java/factory/DBFactory.java
+++ src/main/java/factory/DBFactory.java
@@ -3,15 +3,11 @@ package factory;
 public class DBFactory {
 
     public static ConexionFactory getDB(String tipo,String url,String usuario,String contrasena){
-        switch (tipo){
-            case "mysql":
-                return new MySQLImplementacion(url,usuario,contrasena);
-                case "derby":
-                return new DerbyImplementacion(url,null,null);
-
-            default:
-                return new MySQLImplementacion(url,usuario,contrasena);
-        }
+        return switch (tipo) {
+            case "mysql" -> new MySQLImplementacion(url, usuario, contrasena);
+            case "derby" -> new DerbyImplementacion(url, null, null);
+            default -> new MySQLImplementacion(url, usuario, contrasena);
+        };
     }
 
 }
